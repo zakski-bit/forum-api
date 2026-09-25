fab48689ce250b86bab2471744404e1ed0842142
zakski-bit <05z.abdussalam@gmail.com>
2026-09-25 10:52:53 +0700

    test: fix test to trigger passed CI scenario

--- src/Commons/exceptions/_test/ClientError.test.js
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -2,6 +2,6 @@ import ClientError from '../ClientError.js';
 
 describe('ClientError', () => {
   it('should throw error when directly use it', () => {
-    expect(() => new ClientError('')).toThrowError('deliberate failure');
+    expect(() => new ClientError('')).toThrowError('cannot instantiate abstract class');
   });
 });
